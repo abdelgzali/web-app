6842e9d78b798b32a332fc09c71ee6901855fc79
abdelgzali <abdelgzali@gmail.com>
2018-12-29 19:21:58 -0800

    data is live + dynamic headshots

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -3,10 +3,13 @@
     <input class="search-bar" placeholder="search player" v-model="searchName" @input="findPlayer">
     <div class="player-card">
       <h3>{{player}}</h3>
+      <img v-bind:src="source" alt="head-shot">
       <h4>2018 Season stats (per game):</h4>
+      <p id="value">VALUE: {{value}}</p>
       <p>FG: {{fg}}%</p>
       <p>FT: {{ft}}%</p>
       <p>PPG: {{points}}</p>
+      <p>THREES: {{threes}}</p>
       <p>Assists: {{assists}}</p>
       <p>Rebounds: {{rebounds}}</p>
       <p>Steals: {{steals}}</p>
@@ -27,6 +30,7 @@ export default {
       playersList: [],
       searchName: '',
       player: '',
+      source: 'ballswirl.png',
       fg: '',
       ft: '',
       threes: '',
@@ -36,12 +40,13 @@ export default {
       steals: '',
       blocks: '',
       turnovers: '',
+      value: '',
 
     }
   },
   created: function() {
     // full list of NBA players
-    fetch('https://data.nba.net/10s/prod/v1/2018/players.json')
+    /*fetch('https://data.nba.net/10s/prod/v1/2018/players.json')
       .then((res) => res.json())
       .then((data) => {
         this.playersList = data.league.standard;
@@ -49,7 +54,7 @@ export default {
       })
       .catch((err) => {
         console.log(err);
-      });
+      });*/
 
     // NBA PLAYER STATS & COMPUTED Z-SCORES (WEIGHTED) via FB-Ninja
     // fetch JSON from file
@@ -69,43 +74,46 @@ export default {
         // store data in JSON array
         const sheet = wb.SheetNames[0];
         const playerSheet = wb.Sheets[sheet];
-        const playerJSON = XLSX.utils.sheet_to_json(playerSheet);
-        console.log(playerJSON);
+        this.playersList = XLSX.utils.sheet_to_json(playerSheet);
+        console.log(this.playersList);
       })
   },
   methods: {
     findPlayer() {
       const list = this.playersList;
+      let index = 0;
       list.forEach((player) => {
-        let foundName = player.firstName + " " + player.lastName;
+        let foundName = player.NAME;
         // fuzzy search: foundName.toLowerCase().indexOf(this.searchName.toLowerCase()) !== -1
         if (foundName.toLowerCase() == this.searchName.toLowerCase()) {
           console.log(foundName + " found");
           this.player = foundName;
-          this.getStats(player.personId);
+          this.getStats(index);
+          this.setImg(foundName);
 
-        }
+        };
+        index++;
       })
     },
     getStats(id) {
-      console.log("looking up player");
-      let uri = "https://data.nba.net/10s/prod/v1/2018/players/" + id +"_profile.json";
-      fetch(uri)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          this.fg = data.league.standard.stats.latest.fgp;
-          this.ft = data.league.standard.stats.latest.ftp;
-          this.points = data.league.standard.stats.latest.ppg;
-          this.assists = data.league.standard.stats.latest.apg;
-          this.rebounds = data.league.standard.stats.latest.rpg;
-          this.steals = data.league.standard.stats.latest.spg;
-          this.blocks = data.league.standard.stats.latest.bpg;
-          this.turnovers = data.league.standard.stats.latest.topg;
-        })
-        .catch((err) => {
-          console.log(err);
-        })
+      const list = this.playersList;
+      console.log(list[id]);
+      this.fg = list[id]["FG%"].toFixed(3);
+      this.ft = list[id]["FT%"].toFixed(3);
+      this.threes = list[id]["3P"].toFixed(2);
+      this.points = list[id].PTS.toFixed(2);
+      this.assists = list[id].AST.toFixed(2);
+      this.rebounds = list[id].REB.toFixed(2);
+      this.steals = list[id].STL.toFixed(2);
+      this.blocks = list[id].BLK.toFixed(2);
+      this.turnovers = list[id].TO.toFixed(2);
+      this.value = list[id].VALUE.toFixed(2);
+    },
+
+    setImg(name) {
+      let firstName = name.split(' ').slice(0, -1).join(' ');
+      let lastName = name.split(' ').slice(-1).join(' ');
+      this.source = "https://nba-players.herokuapp.com/players/" + lastName + "/" + firstName;
     }
   }
 }
@@ -145,5 +153,10 @@ $secondary: #1D428A;
 
 .parent-container h3 {
   color: $primary;
+}
+
+#value {
+  color: $primary;
+  font-weight: bold;
 }
 </style>
